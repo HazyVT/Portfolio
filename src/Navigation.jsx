5358584508ad1fbb39bc3b8508567c37392e4d3f
Mohammed Salim <renablehots@gmail.com>
2024-08-09 04:23:16 -0400

    Update Navigation.jsx

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -11,7 +11,7 @@ export default function Navigation() {
   return (
     <Box display='flex' justifyContent='space-between' alignItems='center' padding={4}>
       <Heading cursor={"pointer"} _hover={{color: 'red.300'}} onClick={() => {nav('/')}}>HVT</Heading>
-      <Box display={'flex'} w={'12vw'} justifyContent={'space-around'}>
+      <Box display={'flex'} justifyContent={'space-around'}>
         <Text cursor={"pointer"} _hover={{color: 'red.300'}} onClick={() => {window.location = "/Resume.pdf"}}>Resume</Text>
       </Box>
     </Box>
