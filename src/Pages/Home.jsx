eb62b8316f52e555ee2f904eedaa5a64dff66cb0
Mohammed Salim <renablehots@gmail.com>
2024-11-23 18:17:20 +0400

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import { ScaleFade } from "@chakra-ui/react";
+import { Box, Center, ScaleFade, Text } from "@chakra-ui/react";
 import PropTypes from 'prop-types';
 import { useRef } from "react";
 
@@ -7,9 +7,19 @@ export default function Home({onDesktop}) {
   const portRef = useRef();
   
   return (
+    <Box marginTop={24}>
     <ScaleFade in={true}>
-      
+      <Center>
+      <Box textAlign={'center'}>
+        <Text fontSize={28} fontWeight={600}>Hi. Im Mohammed.</Text>
+        <Text>Programmer | Developer</Text>
+      </Box>
+      <Box>
+        
+      </Box>
+      </Center>
     </ScaleFade>
+    </Box>
 
   )
 }
